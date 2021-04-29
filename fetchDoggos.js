7588bb84996ff565d18ecf37fc5d6d6e2d837383
TheSpaceSloth <83431553+TheSpaceSloth@users.noreply.github.com>
2021-04-29 17:45:02 -0400

    Create fetchDoggos.js
    Simple node js script to use the fetch API to get random photos from the Dog CEO API and download them to a local folder on your computer.

---
+++ fetchDoggos.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+const generateRandomStrings = (length=12)=>Math.random().toString(20).substr(2, length)
+
+function fetchDoggos() {
+  console.log('starting to fetch!')
+  fetch('https://dog.ceo/api/breeds/image/random', {
+    "method": "GET",
+    headers: {'Content-Type': 'application/json'}
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(doggoData) {
+    download(doggoData.message).then(r => console.log(r))
+    async function download(url) {
+      console.log('starting download')
+      const response = await fetch(url);
+      const buffer = await response.buffer();
+      fs.writeFile(`${generateRandomStrings()}.jpg`, buffer, () =>
+          console.log('finished downloading!'));
+    }
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+}
+async function getDogs() {
+  for (let i = 0; i < 10; i++) {
+    await fetchDoggos()
+  }
+}
+getDogs().then(r => console.log(r))
